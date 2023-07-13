26dc9708e60ae52f03a63e88db90e448ba3be3c4
Live session user <rishit@emsec.uk>
2023-07-13 06:03:21 +0000

    Improving directory changes

--- auto/auto.py
+++ auto/auto.py
@@ -18,15 +18,15 @@ def input_data():
 
 def allocate_tables(target_stream):
 
-    kraken_dir = "/home/live/kraken/Kraken"
-    os.chdir(kraken_dir)
+    kraken_dir = "/home/ubuntu/kraken/Kraken"
 
     print("Running kraken")
     c1 = ["sudo", "./kraken", "../indexes/"]
     t1 = subprocess.Popen(c1, stdin=subprocess.PIPE, 
             stdout=subprocess.PIPE, 
             stderr=subprocess.PIPE, 
-            universal_newlines=True)
+            universal_newlines=True, 
+            cwd=kraken_dir)
     
     t1.stdin.write(f"crack {target_stream}" + "\n" + "quit" + "\n")
     t1.stdin.flush()
@@ -44,10 +44,8 @@ def allocate_tables(target_stream):
 
 def find_kc(found, data):
     util_dir = "/home/live/kraken/Utilities"
-    os.chdir(util_dir)
-
     c2 = ["./find_kc", found[1], found[3], data[1], data[2], data[3]]
-    t2 = subprocess.run(c2, capture_output=True, text=True)
+    t2 = subprocess.run(c2, capture_output=True, text=True, cwd=util_dir)
     
     kc_val = ""
     for line in t2.stdout.split("\n"):
